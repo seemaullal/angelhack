b1ddd25e3d6ef9c0958dc5eb520da9ada017f356
melissa <melissaomoy@gmail.com>
2015-07-18 20:28:59 -0400

    Removed all files that are in the .gitignore

--- server/api/twilio/index.js
+++ server/api/twilio/index.js
@@ -2,12 +2,24 @@ var express = require('express'),
   env = require('../../config/local.env.sample.js'),
   twilio = require('twilio')(env.TWILIO_SID, env.TWILIO_KEY),
   config = require('../../config/environment'),
+  User = require('../user.model'),
   router = express.Router();
 
   router.post("/", getMessage);
 
 function getMessage(req, res){
   console.log(req.body);
+  // registration flow
+  User.findOne({phone: req.body.From}, function(err, user) {
+  	if (!user) {
+  		User.create({phone: req.body.From}, function(err, user) {
+  			res.json(200, user);
+  		});
+  	}
+  	else {
+  		console.log("already registered");
+  	}
+  });
 };
 
 module.exports = router;
