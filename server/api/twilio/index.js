b172be5f371679fa4b72096c9bdc103d7d461560
KamillaKhabibrakhmanova <venessiel@gmail.com>
2015-07-19 01:08:35 -0400

    sending day status updates

--- server/api/twilio/index.js
+++ server/api/twilio/index.js
@@ -5,7 +5,8 @@ var express = require('express'),
   User = require('../user/user.model'),
   Thing = require('../thing/thing.model'),
   router = express.Router(),
-  twilioTxt = require('../../components/twilio.js')
+  twilioTxt = require('../../components/twilio.js'),
+  dayCycle = require('../../components/dayCycle.js');
 
 
   router.post("/", getMessage);
@@ -23,7 +24,7 @@ function getMessage(req, res){
         if(err) console.log('error', err);
   			else {
           res.json(200, data);
-          var text = 'Welcome! Please REPLY with the date of the first day of your last period in the format MM-DD. For example, June 01 would be "06-01". If you do not remember the date of your last period, please send a text with the date of the first day of your next period when it next occurs/ If today is the first day of your period, you can reply with "today"';
+          var text = 'Welcome! Please REPLY with the date of the first day of your last period in the format MM-DD. For example, June 01 would be "06-01". If you do not remember the date of your last period, please send a text with the date of the first day of your next period when it next occurs. If today is the first day of your period, you can reply with "today"';
           twilioTxt.sendMessage(user_phone, text);
         }
   		});
@@ -49,13 +50,15 @@ var analyzeBody = function(body, user) {
       var new_date = '2015-' + body;
       user.entryDate = new Date(new_date);
     }
-    response = 'Thanks! We have recorded you period as starting on ' + user.entryDate + '. Wait for your daily text messages to start arriving tomorrow!';
+    response = 'Thanks! We have recorded you period as starting on ' + user.entryDate + '. Wait for your daily text messages to start arriving!';
     user.save(function(err, data){
       if(err) {
         response = 'Sorry, there was an error saving the date. Please try again';
       }
       else {
         console.log(data);
+        var cycleMessage = dayCycle.getMessage(user.entryDate);
+        twilioTxt.sendMessage(user.phone, cycleMessage);
       }
     })
   } else if (body.search(/[?]/) !== -1) {
